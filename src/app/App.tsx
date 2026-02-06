d5c21e60f0a13e0bb974db7335f21af67cb10d56
Nisheglov Sergei <remjest@mail.ru>
2026-02-07 03:05:00 +0700

    add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,20 @@
+import { LoginPage } from '@/pages/LoginPage';
+import { NotFoundPage } from '@/pages/NotFoundPage';
+import { UsersPage } from '@/pages/UsersPage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 export const App = () => {
-    return <div>Check</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/users" element={<UsersPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </Router>
+    )
+
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,42 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  output: {
+mode: 'development',
+entry: './src/index.tsx',
+output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     clean: true,
-  },
-  resolve: {
+},
+resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+    '@': path.resolve(__dirname, 'src'),
     },
-  },
-  module: {
+},
+module: {
     rules: [
-      {
+    {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
-      {
+    },
+    {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-      },
+    },
     ],
-  },
-  plugins: [
+},
+plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+    template: './public/index.html',
     }),
     new CleanWebpackPlugin(),
-  ],
-  devServer: {
+],
+devServer: {
     static: './dist',
     hot: true,
     open: true,
-  },
+    historyApiFallback: true,
+},
 };
