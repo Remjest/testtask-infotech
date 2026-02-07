3967d5f27670744b78deec78ebf10e5b71bcc5b4
Nisheglov Sergei <remjest@mail.ru>
2026-02-08 00:03:37 +0700

    create not-found page

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,3 +1,20 @@
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+
 export const NotFoundPage = () => { 
-    return <>NotFoundPage</>
+
+    const navigate = useNavigate();
+    
+    const handleBack = function () {
+        navigate('/users', { replace: true });
+    }
+    
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Извинете, такой страницы не существует."
+            extra={<Button onClick={handleBack} type="primary">Страница пользователей</Button>}
+        />
+    )
 }
