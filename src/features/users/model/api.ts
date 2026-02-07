2b192d9c29deb4d022f6b9b262a6621713a61248
Nisheglov Sergei <remjest@mail.ru>
2026-02-08 01:36:52 +0700

    add get & show users

---
+++ src/features/users/model/api.ts
@@ -0,0 +1,17 @@
+import { User } from '@/entities/User';
+import axios from 'axios';
+
+const API_URL = 'https://6985d9de6964f10bf254b508.mockapi.io/api/users';
+
+export const getUsers = (): Promise<User[]> => {
+    return axios.get<User[]>(API_URL).then((res) => res.data);
+};
+
+export const createUser = (user: Omit<User, 'id'>): Promise<User> => {
+    return axios.post<User>(API_URL, user).then((res) => res.data);
+};
+
+export const updateUser = (id: string, user: Partial<User>): Promise<User> => {
+    return axios.put<User>(`${API_URL}/${id}`, user).then((res) => res.data);
+};
+
--- src/pages/UsersPage/ui/UsersPage.tsx
+++ src/pages/UsersPage/ui/UsersPage.tsx
@@ -1,3 +1,29 @@
-export const UsersPage = () => { 
-    return <>UsersPage</>
-}
+import { useUsers } from '@/features/users';
+import dayjs from 'dayjs';
+
+export const UsersPage = () => {
+    let {data, isError, isLoading} = useUsers();
+
+    if (isLoading) {
+        return <span>Loading...</span>
+    }
+
+    if (isError) {
+        return <span>Error!</span>
+    }
+
+    return (
+        <div>
+            {data?.map((user) => {
+                return (
+                    <div>
+                        <span>{user.name}</span>
+                        <img src={user.avatar} alt="" />
+                        <span>{dayjs(user.createdAt).format('DD.MM.YYYY')}</span>
+                    </div>
+                )
+                
+            })}
+        </div>
+    );
+};
