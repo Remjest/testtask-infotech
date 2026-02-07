d15c2ad9b6847daa0a0f8f934dc85ed42d7d5c5b
Nisheglov Sergei <remjest@mail.ru>
2026-02-07 22:50:00 +0700

    edit error message

--- src/pages/LoginPage/model/api.ts
+++ src/pages/LoginPage/model/api.ts
@@ -2,7 +2,7 @@ export const login = function (login: string, password: string): Promise<string>
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (login === 'admin' && password === 'admin') resolve('admin-1234-token');
-            else reject(new Error('incorrect username or password'));
+            else reject(new Error('Неправильно введен логин или пароль!'));
         }, 2000)
     })
 }
